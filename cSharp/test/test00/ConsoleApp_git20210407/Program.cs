a64809e2e208543a9de9664a5eb48a608e263663
scorcade <scorcade@outlook.com>
2021-04-06 23:21:40 -0700

    NET

---
+++ cSharp/test/test00/ConsoleApp_git20210407/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp_git20210407
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
