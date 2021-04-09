6247eedcb5830462df0101627506cef7e79fa6a4
scorcade <scorcade@outlook.com>
2021-04-08 19:09:02 -0700

    NET

---
+++ cSharp/test/test00/ConsoleApp3/Program.cs
@@ -0,0 +1,23 @@
+﻿//#define WIN
+using System;
+
+
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int \u005file = 300;
+            Console.WriteLine(\u005file);
+
+            #region
+#if WIN
+//#warning "eeeeeeee"
+            Console.WriteLine("yes");
+#endif
+            #endregion
+        }
+    }
+}
