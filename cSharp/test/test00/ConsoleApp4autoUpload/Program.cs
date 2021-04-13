ab3746ee5fe8fb96069cb84a079d1a15641beea1
scorcade <scorcade@outlook.com>
2021-04-12 23:19:40 -0700

    NET

---
+++ cSharp/test/test00/ConsoleApp4autoUpload/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp4autoUpload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
