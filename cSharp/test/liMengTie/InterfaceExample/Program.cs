5a14f23e105fde9bd705bd7f81cd1bb80666b149
scorcade <scorcade@outlook.com>
2021-04-22 18:27:03 -0700

    NET

---
+++ cSharp/test/liMengTie/InterfaceExample/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+
+namespace InterfaceExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums1 = new int[] { 1, 2, 3, 4, 5 };
+            ArrayList nums2 = new ArrayList { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine(Sum(nums1));
+            Console.WriteLine(Avg(nums1));
+            Console.WriteLine(Sum(nums2));
+            Console.WriteLine(Avg(nums2));
+            //Console.WriteLine("Hello World!");
+
+            static int Sum(IEnumerable nums)
+            {
+                int sum = 0;
+                foreach (var n in nums)
+                {
+                    sum += (int)n;
+                }
+                return sum;
+            }
+            static double Avg(IEnumerable nums)
+            {
+                int sum = 0;double count = 0;
+                foreach (var n in nums)
+                {
+                    sum += (int)n;
+                    count++;
+                }
+                return sum / count;
+            }
+        }
+    }
+}
