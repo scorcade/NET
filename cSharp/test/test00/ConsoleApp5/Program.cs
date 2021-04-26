fc63bfa0d3354052907c33c459d1aefb4e8f94b1
scorcade <scorcade@outlook.com>
2021-04-26 10:44:58 +0800

    NET

---
+++ cSharp/test/test00/ConsoleApp5/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<double, double, double> func1 = (double a, double b) => { return a + b; };
+            var result = func1(100.1, 200.1);
+            
+            Console.WriteLine(result);
+        }
+    }
+}
