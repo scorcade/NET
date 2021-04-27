6bec0f412f8af861239ebdccaa8d5e65335d6f4c
scorcade <scorcade@outlook.com>
2021-04-27 12:14:42 +0800

    NET

---
+++ cSharp/1200/020/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year;
+            string yyy;
+
+            Console.WriteLine("----");
+            yyy = Console.ReadLine();
+            year = Convert.ToInt32(yyy);
+             //bool year = xxx is int;
+
+            if (DateTime.IsLeapYear(yyy))
+            {
+                Console.WriteLine("yyyyyy");
+            }
+            else
+            { Console.WriteLine("nnnn"); }
+
+
+            //if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+            //{
+            //    Console.WriteLine("yyyyyy");
+            //}
+            //else
+            //{ Console.WriteLine("nnnn"); }
+        }
+    }
+}
