396c3811d6fa2eace699a10c5f1f74bc55c73517
scorcade <scorcade@outlook.com>
2021-05-21 10:48:50 +0800

    NET

--- c/test/Project2/Source.c
+++ c/test/Project2/Source.c
@@ -4,27 +4,34 @@
 #include <limits.h>
 
 int main() {
-	int arr[] = { 1,2,3,4,5,1,2,3,4 };
+	int arr[] = { 1,2,3,4,5,1,2,3,4,5,6,6,7,8,7 };
 	int i = 0;
+	int ret = 0;
 	int sz = sizeof(arr) / sizeof(arr[0]);
-	
-	for ( i = 0; i < sz; i++)
-	{
-		int count = 0;
 
-		for (int j = 0; j < sz; j++)
-		{
-			if (arr[i] == arr[j])
-			{
-					count++;
-			}
-		}
-		if (count == 1)
-		{
-			printf("this is %d\n",arr[i]);
-			break;
-		}
+	for (i = 0; i < sz; i++)
+	{
+		ret = ret^arr[i];
 	}
+	printf("this's %d\n",ret);
+
+	//for ( i = 0; i < sz; i++)-------------------
+	//{
+	//	int count = 0;
+
+	//	for (int j = 0; j < sz; j++)
+	//	{
+	//		if (arr[i] == arr[j])
+	//		{
+	//				count++;
+	//		}
+	//	}
+	//	if (count == 1)
+	//	{
+	//		printf("this is %d\n",arr[i]);
+	//		break;
+	//	}
+	//}
 
 
 	return 0;
