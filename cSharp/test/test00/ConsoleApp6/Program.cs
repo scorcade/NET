bca4ff2b6d263a1dacbf07eb2d5a8347a2c6a5bf
scorcade <scorcade@outlook.com>
2021-05-24 12:22:42 +0800

    NET

---
+++ cSharp/test/test00/ConsoleApp6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            //Console.WriteLine("Hello World!");
+            Console.WriteLine(Mydate.Mon);
+
+        }
+
+        enum Mydate
+        {
+            Mon=0,
+            Tue=1,
+            Wed=2,
+        }
+    }
+}
